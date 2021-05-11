68f30b1b17ae7fc4b2d8e5d2256a709a9aa03c7b
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-05-10 23:02:14 -0300

    Em AddMovie.jsx: comenta a importação e o uso de proptypes; descomenta as propriedades "subtitle" e "imagePath" dentro de this.state; altera o nome e implementação do método "onTitleChange" para "onChange", que identifica e altera o estado do respectivo campo que foi alterado; inclusão do atributo "name" dentro dos inputs para facilitar alteração de state no método onChange; implementação dos campos input para subtítulo e imagem.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
   constructor(props) {
@@ -8,44 +8,65 @@ class AddMovie extends Component {
 
     this.state = {
       title: '',
-      // subtitle: '',
-      // imagePath: '',
+      subtitle: '',
+      imagePath: '',
       // storyline: '',
       // rating: 0,
       // genre: 'action',
     };
   }
 
-  onTitleChange = ({ target }) => {
-    this.setState({ title: target.value });
+  onChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
   }
 
   render() {
     // const { onClick } = this.props;
-    const { title } = this.state;
+    const { title, subtitle, imagePath } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label
           htmlFor="title-input"
           data-testid="title-input-label"
         >
-          Título:
+          Título
           <input
             id="title-input"
+            name="title"
             type="text"
             value={ title }
-            onChange={ this.onTitleChange }
+            onChange={ this.onChange }
             data-testid="title-input"
           />
-
+        </label>
+        <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            id="subtitle-input"
+            name="subtitle"
+            type="text"
+            value={ subtitle }
+            onChange={ this.onChange }
+            data-testid="subtitle-input"
+          />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            id="image-input"
+            name="imagePath"
+            value={ imagePath }
+            onChange={ this.onChange }
+            data-testid="image-input"
+          />
         </label>
       </form>
     );
   }
 }
 
-AddMovie.propTypes = {
-  onClick: PropTypes.func,
-}.isRequired;
+// AddMovie.propTypes = {
+//   onClick: PropTypes.func,
+// }.isRequired;
 
 export default AddMovie;
